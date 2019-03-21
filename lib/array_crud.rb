c6f0bce1870fb1978f481b8e85fcaa5a364d5183
leanne strickler <leanne.strickler@gmail.com>
2019-03-21 21:53:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   famous_cats = ["lil' bub", "grumpy cat", "Maru"]
-   
-  famous_cats << "nala cat"
+
+  famous_cats << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)

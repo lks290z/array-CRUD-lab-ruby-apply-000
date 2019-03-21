6cf10a0a91e22214a9a09283da07ffec8bebcc68
leanne strickler <leanne.strickler@gmail.com>
2019-03-21 21:50:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   colors = [ "black", "red", "blue", "green" ]
-  colors << "white"
+  << "white"
 end
 
 def add_element_to_start_of_array(array, element)

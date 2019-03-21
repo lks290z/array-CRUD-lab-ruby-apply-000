b857289e237fd1704abb3728c5730b43f0c208c3
leanne strickler <leanne.strickler@gmail.com>
2019-03-21 21:49:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  numbers << "5"
+  colors + ["black", "red", "blue", "green"]
+  colors << "white"
 end
 
 def add_element_to_start_of_array(array, element)

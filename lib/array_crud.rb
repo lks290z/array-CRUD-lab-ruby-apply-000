274f3d4cce3d37f7755e7edf3bd3a34f90ec584d
leanne strickler <leanne.strickler@gmail.com>
2019-03-21 21:59:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   amous_cats = ["lil' bub", "grumpy cat", "Maru"]
-  arrays! = famous_cats.pop
+  arrays = famous_cats.pop
 end
 
 def remove_element_from_start_of_array(array)

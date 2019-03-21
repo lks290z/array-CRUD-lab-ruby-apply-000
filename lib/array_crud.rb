f638f301c678af24c20c9c22393745d20247e01c
leanne strickler <leanne.strickler@gmail.com>
2019-03-21 21:56:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,11 +13,15 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
+  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
 
+  famous_cats.unshift("arrays!")
 end
 
 def remove_element_from_end_of_array(array)
+  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
 
+  arrays! = famous_cats.pop
 end
 
 def remove_element_from_start_of_array(array)
